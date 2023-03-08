2191bb64ae4ce9b70f3a688d4a99bce3212e3fb7
Mike Izbicki <mike@izbicki.me>
2023-03-08 14:16:27 -0800

    update problems

--- sql/01.sql
+++ sql/01.sql
@@ -1,4 +1,14 @@
 /*
- * Write a SQL query SELECT query that:
- * computes the number of customers who live outside of the US.
+ * You want to watch a movie tonight.
+ * But you're superstitious,
+ * and don't want anything to do with the letter 'F'.
+ *
+ * Write a SQL query that lists the titles of all movies that:
+ * 1) do not have the letter 'F' in their title,
+ * 2) have no actors with the letter 'F' in their names (first or last),
+ * 3) have never been rented by a customer with the letter 'F' in their names (first or last).
+ * 4) have never been rented by anyone with an 'F' in their address (at the street, city, or country level).
+ *
+ * NOTE:
+ * Your results should not contain any duplicate titles.
  */
